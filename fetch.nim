e1c7c6cb0a806fe01e17f4a6f68794bd81f0e924
iliketwertles <67071538+iliketwertles@users.noreply.github.com>
2023-03-24 20:40:38 -0400

    added some stuff to big ascii
    also changed default packages to pacman but can still change to anything

--- fetch.nim
+++ fetch.nim
@@ -13,18 +13,14 @@ proc getDistro(): string =
       return ansiForegroundColorCode(fgCyan) & capitalizeAscii(line.replace("ID=", "")) & ansiResetCode
 
 proc getPackages(): string =
-  if fileExists("/etc/portage/make.conf"):
-    var lineCnt = 0
-    for kind, dir in walkDir("/var/db/pkg"):
-      for kind, pkg in walkDir(dir):
-        inc lineCnt
-    return ansiForegroundColorCode(fgBlue) & $lineCnt & ansiResetCode
+  if fileExists("/etc/pacman.conf"):
+    return ansiForegroundColorCode(fgBlue) & execProcess("pacman -Q | wc -l").strip & ansiResetCode
   else:
     return "fix this bum"
 
 proc getKernel(): string =
   var hostInfo: Utsname
-  let buf = uname(hostInfo)
+  discard uname(hostInfo)
   let kernel = $(cast[cstring](addr hostInfo.release[0]))
   return ansiForegroundColorCode(fgYellow) & kernel & ansiResetCode
 
@@ -60,7 +56,7 @@ case paramCount()
 of 1:
   proc getArch(): string =
     var hostInfo: Utsname
-    let buf = uname(hostInfo)
+    discard uname(hostInfo)
     let cpu = $(cast[cstring](addr hostInfo.machine[0]))
     return ansiForegroundColorCode(fgMagenta) & cpu & ansiResetCode
 
@@ -79,6 +75,30 @@ of 1:
         let gpu = line.find('[')+1..line.rFind(']')-1
         return ansiForegroundColorCode(fgGreen) & line[gpu] & ansiResetCode
 
+  proc getInit(): string =
+    if dirExists("/etc/systemd"):
+      return ansiForegroundColorCode(fgYellow) & "rystemd" & ansiResetCode
+    elif dirExists("/etc/runit"):
+      return ansiForegroundColorCode(fgYellow) & "runit" & ansiResetCode
+    elif fileExists("/etc/rc.conf"):
+      return ansiForegroundColorCode(fgYellow) & "openrc" & ansiResetCode
+    else:
+      return "/shrug"
+
+  proc getShell(): string =
+    if "/usr" in getEnv("SHELL"):
+      return ansiForegroundColorCode(fgMagenta) & getEnv("SHELL").replace("/usr/bin/","") & ansiResetCode
+    else:
+      return ansiForegroundColorCode(fgMagenta) & getEnv("SHELL").replace("/bin/","") & ansiResetCode
+
+  proc getMem(): string =
+    let
+      memoryraw = readLine(open("/proc/meminfo", fmRead))
+      memoryit = memoryraw.find(':')+1..memoryraw.rFind('k')-1
+      memorykb = memoryraw[memoryit].strip()
+      memorymb = parseInt(memorykb) / 1024
+    return ansiForegroundColorCode(fgBlue) & $int(memorymb) & " mb" & ansiResetCode
+
   case paramStr(1)
   of "-p", "--pickle":
     let ani_ascii = fmt("""
@@ -94,14 +114,14 @@ of 1:
 ⠀⠀⠀⠀⠀⠀⢀⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⡸⠀⠀⠀     Arch: {getArch()}
 ⠀⠀⠀⠀⠀⠀⡾⣖⠀⠀⠀⠀⠀⠀⠀⢠⠀⠴⠃⢠⠁⠀⠀⠀     CPU: {getCPU()}
 ⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠆⠀⠀⢀⠎⠀⠀⠀⠀     GPU: {getGPU()}
-⠀⠀⠀⢰⠊⠀⠀⡀⠀⠀⠀⠀⠀⠀⠌⢠⠁⠀⡼⠀⠀⠀⠀⠀
-⠀⠀⢠⠋⠀⢰⡖⠁⠀⠠⠀⠀⠀⡌⢠⠃⠀⡜⠀⠀⠀⠀⠀⠀
-⠀⢀⠻⡍⠀⠀⠀⠀⡀⠁⠀⢀⠌⠠⠃⠀⡜⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢰⠊⠀⠀⡀⠀⠀⠀⠀⠀⠀⠌⢠⠁⠀⡼⠀⠀⠀⠀⠀     Init: {getInit()}
+⠀⠀⢠⠋⠀⢰⡖⠁⠀⠠⠀⠀⠀⡌⢠⠃⠀⡜⠀⠀⠀⠀⠀⠀     Shell: {getShell()}
+⠀⢀⠻⡍⠀⠀⠀⠀⡀⠁⠀⢀⠌⠠⠃⠀⡜⠀⠀⠀⠀⠀⠀⠀     Mem: {getMem()}
 ⠀⡎⢰⠁⠀⠀⠀⡀⠀⠀⠠⠃⢔⠁⢀⠎⠁⠀⠀⠀⠀⠀⠀⠀
 ⢰⢀⠃⠀⠢⠠⠐⠀⠀⡠⠀⠈⠁⡠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠈⣼⠀⠀⠀⠀⠀⠀⠜⡀⠀⢠⠜⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠘⠣⣤⠀⣀⢔⠡⣊⠠⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀     You installed {getInstallTime().replace("-", "")} ago
 """)
     echo ani_ascii
   of "-f", "--fumo":
@@ -118,14 +138,14 @@ of 1:
 ⠀⠀⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⡿⣿⣿⣿⣿⣷⣳⢯⡼⣌⢞⡵⢣⣮⣞⡰⣤⣹⣵⣶⣿⣿⣿⣿⠷⣽⣿⣽⡿⠀⠀⠀⠀⠀⠀       Arch: {getArch()}
 ⠀⠀⠀⠀⠈⠉⠙⠛⠛⠛⠛⠛⠉⣿⣷⣉⢿⣿⣿⣿⣿⢦⣷⣭⣮⣱⣆⣶⣌⣷⠤⣛⣿⣿⣿⣿⣿⢿⡐⣿⣿⡙⠃⠀⠀⠀⠀⠀⠀       CPU: {getCPU()}
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⠟⠟⠒⢻⢏⢯⣛⣾⣽⣿⣿⣿⣿⣿⣿⣿⣼⡗⣸⢍⠻⡛⡜⢦⡑⠛⠳⡁⠀⠀⠀⠀⠀⠀⠀       GPU: {getGPU()}
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠮⡌⠂⢎⢼⣿⣭⣫⣿⡿⣿⣿⣿⣿⣇⡿⣌⠳⣄⡈⢇⠈⣁⠂⠀⡀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣀⣤⣦⡞⡀⠀⡀⠄⡐⠤⣁⢔⣠⣹⣦⣿⣿⣿⣿⣿⣛⣿⣿⣿⣿⣿⣷⣯⢷⠾⣟⡷⣜⡔⡜⡠⡐⡁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠮⡌⠂⢎⢼⣿⣭⣫⣿⡿⣿⣿⣿⣿⣇⡿⣌⠳⣄⡈⢇⠈⣁⠂⠀⡀⠀⠀⠀⠀⠀⠀       Init: {getInit()}
+⠀⠀⠀⠀⠀⠀⣀⣤⣦⡞⡀⠀⡀⠄⡐⠤⣁⢔⣠⣹⣦⣿⣿⣿⣿⣿⣛⣿⣿⣿⣿⣿⣷⣯⢷⠾⣟⡷⣜⡔⡜⡠⡐⡁⠀⠀⠀⠀⠀       Shell: {getShell()}
 ⠀⠀⠀⡠⠒⠛⠛⠻⣏⡴⠛⠛⠓⢾⢷⠓⠒⠚⠛⢿⣿⣿⡟⠛⠛⠛⠻⣿⣿⠛⠛⠛⣿⡟⠛⠛⢶⠛⠛⠚⣷⠓⠓⣾⠓⢲⠀⠀⠀
 ⠀⠀⠀⡇⠀⢸⠀⢀⣿⠁⠀⣿⠀⠈⣿⠀⠀⡇⠀⢸⣿⣿⡇⠀⢰⡆⠀⢿⡇⠀⡀⠀⣧⡇⠀⠀⢸⠀⠀⠀⣿⠀⠀⢹⠀⢸⠀⠀⠀
 ⠀⠀⠀⡇⠀⢸⠉⠉⣿⠀⠀⣿⠀⠀⣿⠀⠀⡏⠀⢸⣂⠙⡇⠀⢸⡅⠀⣻⠇⠀⣇⠀⢸⡇⠀⢄⠀⠀⠆⠀⣿⠀⢀⠀⠀⢸⠀⠀⠀
 ⠀⠀⠀⡇⠀⢸⠀⠀⡿⡄⠀⠿⠀⢠⣿⠀⠀⠏⠀⢸⣦⣥⡇⠀⠸⠁⠀⣿⠀⠀⣀⠀⠸⡇⠀⣸⠀⢸⡁⠀⣿⠀⢸⡆⠀⢸⠀⠀⠀
 ⠀⠀⠀⠙⠦⠤⠶⠤⠇⠑⠦⠤⣴⣾⣿⣤⣤⣤⣴⣮⠋⠉⠧⠤⠤⠤⠖⠻⠤⠤⠻⠤⠤⠧⠤⣼⣤⣼⣥⣤⣿⠤⠼⠳⠤⠼⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⢿⢿⡿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⠿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠛⢿⢿⡿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⠿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀       You installed {getInstallTime().replace("-", "")} ago
 """)
     echo fumo_ascii
   of "-b", "--big":
@@ -133,20 +153,20 @@ of 1:
     let beeg_ascii = fmt("""
         a8888b.
        d888888b.
-       8P"YP"Y88     Distro: {getDistro()}
-       8|o||o|88     Kernel: {getKernel()}
-       8'    .88     Desktop: {getDesktop()}
-       8`._.' Y8.    Packages: {getPackages()}
-      d/      `8b.   Arch: {getArch()}
-     dP   .    Y8b.  CPU: {getCPU()}
-    d8:'  "  `::88b  GPU: {getGPU()}
-   d8"         'Y88b
-  :8P    '      :888
+       8P"YP"Y88      Distro: {getDistro()}
+       8|o||o|88      Kernel: {getKernel()}
+       8'    .88      Desktop: {getDesktop()}
+       8`._.' Y8.     Packages: {getPackages()}
+      d/      `8b.    Arch: {getArch()}
+     dP   .    Y8b.   CPU: {getCPU()}
+    d8:'  "  `::88b   GPU: {getGPU()}
+   d8"         'Y88b  Init: {getInit()}
+  :8P    '      :888  Shell: {getShell()}
    8a.   :     _a88P
 ._/"Yaa_:   .| 88P|
 \    YP"    `| 8P  `.
 /     \.___.d|    .'
-`--..__)8888P`._.'       
+`--..__)8888P`._.'    You installed {getInstallTime().replace("-", "")} ago
 """)
     echo beeg_ascii
 else:
